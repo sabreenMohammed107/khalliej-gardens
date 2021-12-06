1d368c54f1e82b8824570ba72d336292b107070a
sabreenMohammed107 <sabreenm312@gmail.com>
2021-12-06 18:54:54 +0200

    test

--- resources/views/web/services.blade.php
+++ resources/views/web/services.blade.php
@@ -34,14 +34,14 @@
                                     @foreach ( $cat->service as $service)
 									<div class="col-lg-4 col-md-6 single-blog pb-30">
 										<div class="thumb hover14 ">
-											<figure><img class="img-fluid" style="height: 280PX" src="{{ asset('uploads/service') }}/{{ $service->image }}" alt=""></figure>
+											<figure><img class="img-fluid" style="height: 230PX" src="{{ asset('uploads/service') }}/{{ $service->image }}" alt=""></figure>
 										</div>
 										<div class="text-box">
 											<a href="{{ LaravelLocalization::localizeUrl('/single-service/' . $service->id) }}">
 												<h4 class="clr-green" style="font-size: 20px"> {{ $service->ar_name }}</h4>
 											</a>
 											<p>
-                                                {!! Illuminate\Support\Str::limit($service->ar_desc ?? '', $limit = 80, $end = '...') !!}
+                                                {!! Illuminate\Support\Str::limit($service->ar_desc ?? '', $limit = 100, $end = '...') !!}
 											</p>
 											<h4><a href="{{ LaravelLocalization::localizeUrl('/single-service/' . $service->id) }}" class="clr-green">إقراء المزيد &gt;&gt;</a></h4>
 										</div>
