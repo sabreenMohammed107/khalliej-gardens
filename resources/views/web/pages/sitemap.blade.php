743c6af49002e134ef62a890aa18f561ce0f5204
sabreenMohammed107 <sabreenm312@gmail.com>
2022-01-10 14:23:14 +0200

    tt

--- resources/views/web/pages/sitemap.blade.php
+++ resources/views/web/pages/sitemap.blade.php
@@ -3,32 +3,32 @@
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>http://khalliej-gardens.senior-consultingco.com/</loc>
+        <loc>http://gulfgardens.net/</loc>
     </url>
     <url>
-        <loc>http://khalliej-gardens.senior-consultingco.com/about</loc>
+        <loc>http://gulfgardens.net/about</loc>
     </url>
     <url>
-        <loc>http://khalliej-gardens.senior-consultingco.com/web-service</loc>
+        <loc>http://gulfgardens.net/web-service</loc>
     </url>
     <url>
-        <loc>http://khalliej-gardens.senior-consultingco.com/web-gallery</loc>
+        <loc>http://gulfgardens.net/web-gallery</loc>
     </url>
     <url>
-        <loc>http://khalliej-gardens.senior-consultingco.com/web-blogs</loc>
+        <loc>http://gulfgardens.net/web-blogs</loc>
     </url>
     <url>
-        <loc>http://khalliej-gardens.senior-consultingco.com/contact</loc>
+        <loc>http://gulfgardens.net/contact</loc>
     </url>
     @foreach($blogs as $blog)
         <url>
-            <loc>http://khalliej-gardens.senior-consultingco.com/single-blog/{{ $blog->id }}</loc>
+            <loc>http://gulfgardens.net/single-blog/{{ $blog->id }}</loc>
         </url>
     @endforeach
 
     @foreach($services as $service)
     <url>
-        <loc>http://khalliej-gardens.senior-consultingco.com/single-service/{{ $service->id }}</loc>
+        <loc>http://gulfgardens.net/single-service/{{ $service->id }}</loc>
     </url>
 @endforeach
 </urlset>
